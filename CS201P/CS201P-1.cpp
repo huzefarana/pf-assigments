d20a7b8d9b0a990cf6fbea3f5748d4dfa9f200ce
huzefarana <imhuzefarana@gmail.com>
2024-04-28 00:56:01 +0500

    lab assigment, folder structure

---
+++ CS201P/CS201P-1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * Calculates the sum of the even digits in a given number.
+ * 
+ * @param evenDigits The number from which to calculate the sum of even digits.
+ * @return The sum of the even digits in the given number.
+ */
+
+int sumOfEvenDigits(int evenDigits){
+    int sum = 0;
+    while(evenDigits > 0){
+        sum += evenDigits % 10;
+        evenDigits /= 10;
+    }
+    return sum;
+}
+
+int main(){
+    string studentId = "bc12346789"; //student id
+    int digitalPartsOfStudentId = 12346789;
+    int evenDigitsOfStudentId = 2468;
+    int sum = sumOfEvenDigits(evenDigitsOfStudentId);
+    cout << "Student ID:" << studentId << "\n";
+    cout << "Digital part of the student ID is: " << digitalPartsOfStudentId << "\n";
+    cout << "Even Digits: " << evenDigitsOfStudentId << "\n";
+    cout << "Sum of Even Digits is: " << sum;
+}
---
+++ CS201P/README.md
@@ -0,0 +1,11 @@
+Write a C++ program that performs the following tasks: 
+
+Your program must take the numerical part of your Student ID BC123456789 which is 123456789.
+•	Display your Student ID
+•	Display the digital part of your Student ID
+•	Store the numerical part of your Student ID in a variable
+•	Separate the even digits
+•	Calculate the sum of all the even digits
+•	Print the sum of all the even digits on the screen
+
+You will define the sumOfEvenDigits( ) function to calculate the sum of all the even digits.
